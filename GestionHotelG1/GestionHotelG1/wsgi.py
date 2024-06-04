d0298e4c10588182e145aeb12de74672162578db
Jxel117 <166523314+Jxel117@users.noreply.github.com>
2024-06-03 19:59:08 -0500

    Cod_Inicial
    Creación carpeta de cod1

--- GestionHotelG1/GestionHotelG1/wsgi.py
+++ GestionHotelG1/GestionHotelG1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GestionHotelG1 project.
+WSGI config for HolaMundo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestionHotelG1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HolaMundo.settings')
 
 application = get_wsgi_application()
